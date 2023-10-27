4194ec64b80f0f4f10ba18acd95f1aeedad548b1
Skye <Skye@supermind.us>
2023-10-27 01:53:31 -0400

    updated profile.ejs and controller profile.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
+const { resolveInclude } = require('ejs');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const profile = require('/controllers/profile');
 
 const userSchema = new Schema({
   name: String,
@@ -8,10 +10,14 @@ const userSchema = new Schema({
     required: true
   },
   email: String,
-  avatar: String
-}, {
+  avatar: String,
+}, 
+{
   timestamps: true
 });
-
+//const profileSchema = new Schema({
+//  res.render(profile)
+//})
 
 module.exports = mongoose.model('User', userSchema);
+//module.exports = mongoose.model('User', profileSchema);
